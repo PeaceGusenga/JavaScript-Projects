d985b25984f4ba7c6ceb33b53232d6b291a58c76
PeaceGusenga <gusengapeace@gmail.com>
2023-04-19 10:08:35 +0200

    exclude duplicate and include spaces method

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -17,20 +17,40 @@ const characters = {
 const generatePassword = () => {
     let staticPassword = "",
     randomPassword = "",
+    excludeDuplicate = false;
     passLength = lengthSlider.value;
 
     //looping through each options checkbox
     options.forEach(option =>{ 
         // if checkbox is checked
         if(option.checked) { 
-
-            staticPassword += characters[option.id]; 
+            // if exlude duplicate and or include spaces is notchecked
+            if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+                staticPassword += characters[option.id];   
+            }else if(option.id === "spaces"){ // if inlcude spaces is checked
+                staticPassword += ' ${staticPassword} '; // add space to beggining and end of statsic password
+            }else{ //if exlcude duplicates is checked
+                excludeDuplicate = true;
+            }
+            
         }
     });
+
+    // for loop to generate random password based on slider value
     for (let i = 0; i < passLength; i++) {
-        
-        randomPassword += staticPassword[Math.floor(Math.random() * staticPassword.length)];
-        
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        // exlcude duplicate values logic
+        if (excludeDuplicate) {
+            // if randomPassword doesn't contain the current random character or randomChar is equal
+            //to space " ", then add random character to randomPassword else decrement i by -1
+            if (!randomPassword.includes(randomChar) || randomChar == " ") {
+                randomPassword += randomChar;
+            } else {
+                i--;
+            }
+        } else { // else add random character to randomPassword
+            randomPassword += randomChar;
+        }
     }
 }
 updateSlider();
